bc42653abc07293ee18826dbbc1b581ce48394e8
basilgo-jay <21102868@usc.edu.ph>
2026-03-18 04:46:28 +0800

    Add BMI Calculator MAUI App Project

---
+++ BMI Calculator App/BMI Calculator App/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace BMI_Calculator_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalculateClicked(object? sender, EventArgs e)
+        {
+            try
+            {
+                double height = Convert.ToDouble(HeightEntry.Text);
+                double weight = Convert.ToDouble(WeightEntry.Text);
+
+                double bmi = weight / (height * height);
+
+                string category = "";
+
+                if (bmi < 18.5)
+                    category = "Underweight";
+                else if (bmi < 24.9)
+                    category = "Normal weight";
+                else if (bmi < 29.9)
+                    category = "Overweight";
+                else
+                    category = "Obese";
+
+                ResultLabel.Text = $"BMI: {bmi:F2}\nCategory: {category}";
+            }
+            catch
+            {
+                ResultLabel.Text = "Please enter valid numeric values.";
+            }
+        }
+    }
+}
